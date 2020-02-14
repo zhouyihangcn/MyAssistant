7d2aea823a8c59c94ca791f0015c3e725853fcfe
zhouyihang <zhouyihang@windows10.microdone.cn>
2020-02-14 09:02:20 +0800

    constant

--- src/main/java/com/zyh/wx/assistant/util/Constant.java
+++ src/main/java/com/zyh/wx/assistant/util/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 	public static final String ZONE_ID = "Asia/Shanghai";
 	public static final int DEFAULT_OFFSET_HOURS = 8;
 	public static final int DEFAULT_OFFSET_MINUTES = 0;
-	public static final int MAX_CONTNET_LENGTH_SAVE = 10;
-	public static final int MAX_CONTNET_LENGTH_WX_RESP = 200;
+	public static final int MAX_CONTNET_LENGTH_SAVE = 1000;
+	public static final int MAX_CONTNET_LENGTH_WX_RESP = 2000;
 }
